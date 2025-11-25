d6bd935f0e2495bd77116c4090d340141ff27105
dudow <edu30.nm@gmail.com>
2025-11-25 20:18:46 -0300

    Enhance AuthService: add password hashing and verification methods

--- nest-api/src/auth/auth.service.ts
+++ nest-api/src/auth/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor() {}
 
   async signIn(): Promise<any> {}
+
+  async hashPassword(password: string): Promise<string> {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    return hashedPassword;
+  }
+
+  async verifyPassword(password: string, hash: string): Promise<boolean> {
+    const isMatch = await bcrypt.compare(password, hash);
+
+    return isMatch;
+  }
 }
