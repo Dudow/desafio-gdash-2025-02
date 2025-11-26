e532f69e78bba3ebb52732b7f767f213c5607aad
dudow <edu30.nm@gmail.com>
2025-11-26 12:34:41 -0300

    feat: nest routes in layouts

--- react-dashboard/src/App.tsx
+++ react-dashboard/src/App.tsx
@@ -1,34 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { AuthProvider } from "@contexts/authContext/auth-provider";
 import ProtectedRouteHandler from "@components/auth/ProtectedRouteHandler";
-import ExplorePage from "./pages/explorePage";
-import LoginPage from "./pages/login";
-import { Toaster } from "@/components/ui/sonner";
+import ExplorePage from "@pages/explorePage";
+import LoginPage from "@pages/login";
+import { Toaster } from "@components/ui/sonner";
+import { AppLayoutLogged } from "@/layouts/layoutLogged";
+import { AppLayoutDefault } from "@/layouts/layoutDefault";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRouteHandler>
-                <div>Dashboard</div>
-              </ProtectedRouteHandler>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <ProtectedRouteHandler>
-                <div>Users</div>
-              </ProtectedRouteHandler>
-            }
-          />
+          {/* Common Routes */}
+          <Route element={<AppLayoutDefault />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/explore" element={<ExplorePage />} />
+          </Route>
 
-          <Route path="/explore" element={<ExplorePage />} />
+          {/* Protected Routes */}
+          <Route
+            element={
+              <ProtectedRouteHandler>
+                <AppLayoutLogged />
+              </ProtectedRouteHandler>
+            }
+          >
+            <Route path="/users" element={<div>Users</div>} />
+
+            <Route path="/" element={<div>Dashboard</div>} />
+          </Route>
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
