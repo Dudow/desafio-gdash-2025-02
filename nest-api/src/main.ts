9fb3abf173eedb6f453f08bfddcded4610cad4a1
dudow <edu30.nm@gmail.com>
2025-12-07 20:33:09 -0300

    feat: improve CORS configuration by adding frontend URL support

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -10,14 +10,21 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(cookieParser());
+  const configService = new ConfigService();
+
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
 
   const allowedOrigins = ['http://localhost:5173', 'http://localhost:3005'];
+
+  if (frontendUrl) {
+    allowedOrigins.push(frontendUrl);
+  }
+
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
   });
 
-  const configService = new ConfigService();
   const USER_DEFAULT_EMAIL = configService.get<string>('DEFAULT_USER_EMAIL');
   const DEFAULT_USER_PASSWORD = configService.get<string>(
     'DEFAULT_USER_PASSWORD',
