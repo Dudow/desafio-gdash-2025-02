3653b04f9956437ce0ea3313ab0ec4c7fc9be808
dudow <edu30.nm@gmail.com>
2025-12-07 22:27:34 -0300

    feat: increase timeout

--- react-dashboard/src/services/api.ts
+++ react-dashboard/src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3004/api",
-  timeout: 10000,
+  timeout: 30000,
   withCredentials: true,
 });
 
